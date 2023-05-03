0028e505bb41736653b1a98f4f08db6b8735f15a
raul.argemi.7e6 <raul.argemi.7e6@itb.cat>
2023-05-03 09:08:55 +0200

    Profile

---
+++ src/views/Profile.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav-bar/>
+    <FormulariUser/>
+  </template>
+  
+  <script>
+  import FormulariUser from "@/components/FormulariUser.vue";
+  import NavBar from "@/components/Navbar.vue";
+  
+  export default {
+    name: "Profile",
+    components: { NavBar, FormulariUser },
+  }
+  
+  </script>
+  
+  <style scoped>
+
+  </style>
