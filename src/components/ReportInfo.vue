d9b9c2a8cf4e43d9f6b574a2e17919794e993d2e
m-gusta-666 <martigustamante@gmail.com>
2023-05-06 15:42:52 +0200

    Added treasure reports from API to Management page

---
+++ src/components/ReportInfo.vue
@@ -0,0 +1,92 @@
+<template>
+
+  <div class="report">
+    <div class="report2">
+      <div class="username-date">
+        <!--        <h4> {{ user.username }} - {{ review.solved }} </h4> -->
+        <h4>Reported by: {{ user.nickName }} - Date: {{ report.reportDate }} </h4>
+      </div>
+      <div class="report-text">
+        <p v-if="report.reportInfo.length > 300" class="report-text-scroll">{{ report.reportInfo }}</p>
+        <p v-else class="report-text-normal">{{ report.reportInfo }}</p>
+      </div>
+      <!--      <p>{{ review.path }}</p>-->
+    </div>
+  </div>
+</template>
+
+<script>
+
+import UserService from "@/services/user.service";
+
+export default {
+  name: "ReportInfo",
+  data() {
+    return {
+      user: ""
+    }
+  },
+  props: {
+    report: Object,
+  },
+  mounted() {
+    UserService.getById(this.report.idUser).then((response) => {
+      this.user = response.data
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
+
+<style scoped>
+.report {
+  display: flex;
+  margin: 20px 0;
+  background: #a0deb1;
+  border: 4px solid #84b893;
+  width: 45%;
+  margin: auto;
+  border-radius: 25px;
+}
+
+
+.report2 {
+  width: 100%;
+}
+
+.report2 p {
+  margin: 2rem;
+  align-items: center;
+  text-align: left;
+}
+
+.username-date {
+  display: flex;
+  justify-content: space-around;
+}
+
+.username-rating h4 {
+  margin-right: 10px;
+  font-weight: bold;
+  width: 200px;
+  text-align: left;
+
+}
+
+
+.report-text {
+  max-height: 150px;
+  overflow-y: scroll;
+}
+
+.report-text-normal {
+  margin: 0;
+}
+
+.report-text-scroll {
+  margin: 0;
+  white-space: pre-line;
+  overflow-wrap: anywhere;
+}
+</style>
--- src/views/Management.vue
+++ src/views/Management.vue
@@ -1,6 +1,11 @@
 <template>
   <nav-bar/>
   <FormulariTreasure v-bind:id="this.$route.params.idTreasure"/>
+  <div v-if="this.$route.params.idTreasure!==''">
+    <ReportInfo v-for="(report, index) of reports" v-bind:key="index" v-bind:report="report"/>
+    <h3 v-if="reports===''">This treasure has not been reported</h3>
+  </div>
+
   <Footer/>
 </template>
 
@@ -8,19 +13,34 @@
 import FormulariTreasure from "@/components/FormulariTreasure.vue";
 import Footer from "@/components/Footer.vue";
 import NavBar from "@/components/Navbar.vue";
+import ReportInfo from "@/components/ReportInfo.vue";
+import ReportService from "@/services/report.service";
 
 export default {
   name: "ManagementView",
-  components: {NavBar, Footer, FormulariTreasure},
+  components: {NavBar, Footer, FormulariTreasure, ReportInfo},
+  data() {
+    return {
+      reports: ""
+    }
+
+  },
   props: {
     id: String
   },
   watch: {
     '$route.query': {
-    handler: function (){
-      location.reload()
+      handler: function () {
+        location.reload()
+      }
     }
-}
+  },
+  mounted() {
+    ReportService.getAllByTreasure(this.$route.params.idTreasure).then((response) => {
+      this.reports = response.data
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 }
 
