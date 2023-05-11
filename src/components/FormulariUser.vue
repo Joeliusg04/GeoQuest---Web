df36e1691e1370b7836ceca2a8941a10697cf2ae
m-gusta-666 <martigustamante@gmail.com>
2023-05-11 12:46:38 +0200

    Added location reload after updating user

--- src/components/FormulariUser.vue
+++ src/components/FormulariUser.vue
@@ -129,9 +129,11 @@ export default {
         formData.append('body', JSON.stringify(this.user))
         UserService.update(formData, this.user.idUser).then((response) => {
           this.user = response.data
+          location.reload()
         }).catch((error) => {
           console.log(error)
         })
+
       }
     }
     ,
